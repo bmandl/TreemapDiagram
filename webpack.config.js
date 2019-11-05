2a4df81dcbcd9d06fb833fca9ae2b9a7d00a8330
Blaž Mandl <blaz.mandl@gmail.com>
2019-11-05 20:09:27 +0100

    initial commitment

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    devServer: {
+        contentBase: './dist',
+        port: 8888,
+        // Send API requests on localhost to API server get around CORS.
+        proxy: {
+            '/api': {
+                target: {
+                    host: "0.0.0.0",
+                    protocol: 'http:',
+                    port: 8080
+                },
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                ],
+            },
+        ],
+    },
+};
