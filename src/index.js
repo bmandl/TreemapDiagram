813bdebcd420ea887d78f300bf47d793c0bf8aa0
Blaž Mandl <blaz.mandl@gmail.com>
2019-11-10 09:28:35 +0100

    SVG and treemap finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 import * as d3 from "d3";
-import * as topojson from "topojson";
 import './style.css';
+
+const dataset = "https://cdn.freecodecamp.org/testable-projects-fcc/data/tree_map/kickstarter-funding-data.json"
+
+var margin = { top: 10, right: 10, bottom: 10, left: 10 },
+    width = 1200 - margin.left - margin.right,
+    height = 580 - margin.top - margin.bottom;
+
+
+const title = d3.select("body").append("div")
+    .attr("class", "title");
+title.append("h1")
+    .attr("id", "title")
+    .text("Kickstarter funding");
+title.append("h2")
+    .attr("id", "description")
+    .text("Top 100 kickstarter fundings grouped by category");
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+/*.append("g")
+.attr("transform",
+    "translate(" + margin.left + "," + margin.top + ")");*/
+
+d3.json(dataset).then(data => {
+
+    var root = d3.hierarchy(data).sum(d => d.value);
+    d3.treemap()
+        .size([width, height])
+        .paddingInner(1)
+        (root);
+
+    var cell = svg.selectAll("g")
+        .data(root.leaves())
+        .enter().append("g")
+        .attr("class", "group")
+        .attr("transform", function (d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+
+    cell.append("rect")
+        .attr("class", "tile")
+        .attr('width', d => d.x1 - d.x0)
+        .attr('height', d => d.y1 - d.y0)
+        .style("stroke", "black")
+        .style("fill", "slateblue")
+
+    cell.append("text")
+        .attr('class', 'tile-text')
+        .selectAll("tspan")
+        .data(d => d.data.name.split(/(?=[A-Z][^A-Z])/g))
+        .enter().append("tspan")
+        .attr("x", 4)
+        .attr("y", (d, i) => 13 + i * 10)
+        .text(d => d);
+});
