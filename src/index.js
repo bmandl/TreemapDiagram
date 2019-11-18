b1af1b7f241586af61d5865a93abece92f0bea1a
Blaž Mandl <blaz.mandl@gmail.com>
2019-11-18 19:15:30 +0100

    finished legend

--- src/index.js
+++ src/index.js
@@ -24,26 +24,26 @@ var svg = d3.select("body").append("svg")
 .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");*/
 var colors = [
-	"#fdb664",
-	"#0030ac",
-	"#65cc00",
-	"#ef6cff",
-	"#008400",
-	"#ff4ed7",
-	"#004900",
-	"#ca0000",
-	"#00ffff",
-	"#a80027",
-	"#00deff",
-	"#760000",
-	"#00d2ff",
-	"#660000",
-	"#ffe36b",
-	"#003e97",
-	"#ffca81",
-	"#0063a0",
-	"#1d3e08",
-	"#004859"
+    "#fdb664",
+    "#0030ac",
+    "#65cc00",
+    "#ef6cff",
+    "#008400",
+    "#ff4ed7",
+    "#004900",
+    "#ca0000",
+    "#00ffff",
+    "#a80027",
+    "#00deff",
+    "#760000",
+    "#00d2ff",
+    "#660000",
+    "#ffe36b",
+    "#003e97",
+    "#ffca81",
+    "#0063a0",
+    "#1d3e08",
+    "#004859"
 ];
 
 var colorScale = d3.scaleOrdinal(colors);
@@ -51,7 +51,16 @@ var colorScale = d3.scaleOrdinal(colors);
 
 var legend = d3.select("body").append("svg")
     .attr("id", "legend")
-    .append("g");
+    .attr("width", 500);
+
+var legendWidth = 500;
+const LEGEND_OFFSET = 10;
+const LEGEND_RECT_SIZE = 15;
+const LEGEND_H_SPACING = 150;
+const LEGEND_V_SPACING = 10;
+const LEGEND_TEXT_X_OFFSET = 3;
+const LEGEND_TEXT_Y_OFFSET = -2;
+var legendElemsPerRow = Math.floor(legendWidth / LEGEND_H_SPACING);
 
 d3.json(dataset).then(data => {
 
@@ -61,13 +70,16 @@ d3.json(dataset).then(data => {
         .paddingInner(1)
         (root);
 
+    var categories = root.leaves().map(nodes => nodes.data.category);
+    categories = categories.filter((category, index, self) => self.indexOf(category) === index);
+
     colorScale.domain(root.data.children.map(d => d.name));
-    console.log(colorScale.range());
+
     var cell = svg.selectAll("g")
         .data(root.leaves())
         .enter().append("g")
         .attr("class", "group")
-        .attr("transform", function (d) { return "translate(" + d.x0 + "," + d.y0 + ")"; });
+        .attr("transform", d => `translate(${d.x0},${d.y0})`);
 
     cell.append("rect")
         .attr("class", "tile")
@@ -86,5 +98,28 @@ d3.json(dataset).then(data => {
         .enter().append("tspan")
         .attr("x", 4)
         .attr("y", (d, i) => 13 + i * 10)
+        .text(d => d);
+
+    var legendElem = legend
+        .append("g")
+        .attr("transform", `translate(60,${LEGEND_OFFSET})`)
+        .selectAll("g")
+        .data(categories)
+        .enter().append("g")
+        .attr("transform", (d, i) => {
+            return `translate(${((i % legendElemsPerRow) * LEGEND_H_SPACING)},
+            ${((Math.floor(i / legendElemsPerRow)) * LEGEND_RECT_SIZE +
+                    (LEGEND_V_SPACING * (Math.floor(i / legendElemsPerRow))))})`;
+        });
+
+    legendElem.append("rect")
+        .attr('width', LEGEND_RECT_SIZE)
+        .attr('height', LEGEND_RECT_SIZE)
+        .attr('class', 'legend-item')
+        .attr('fill', d => colorScale(d));
+
+    legendElem.append("text")
+        .attr('x', LEGEND_RECT_SIZE + LEGEND_TEXT_X_OFFSET)
+        .attr('y', LEGEND_RECT_SIZE + LEGEND_TEXT_Y_OFFSET)
         .text(d => d);
 });
